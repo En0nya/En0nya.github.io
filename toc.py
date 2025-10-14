0c2d55be1609fc14a1b1cab90aaa4790529de98c
En0nya <black_trees@qq.com>
2025-10-14 22:30:40 +0800

    Auto update :2025-10-14 22:30:40

--- toc.py
+++ toc.py
@@ -98,7 +98,7 @@ def generate_directory_index(md_files, root_dir):
         for file_info in files_in_dir:
             html_path = file_info['path'].replace('.md', '.html').replace('.markdown', '.html')
             html_path = "posts\\" + html_path
-            content.append(f"- [⌈{file_info['title']}⌋]({html_path})    *`{file_info['mod_time']}`*")
+            content.append(f"- [⌈{file_info['title']}⌋]({html_path})  最后更新时间：*`{file_info['mod_time']}`*")
         content.append("")
     
     return "\n".join(content)
